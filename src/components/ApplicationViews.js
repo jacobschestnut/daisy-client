7da23dfd5d5927274eb884fe96df4993daa41633
Jake Chestnut <jacobschestnut@gmail.com>
2022-09-03 13:59:34 -0500

    recipe displaying correctly

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,10 +1,11 @@
 import React from "react"
 import { Route } from "react-router-dom"
 import { CocktailList } from "./cocktail/CocktailList"
+import { CocktailDetails } from "./cocktail/CocktailDetails"
 
 export const ApplicationViews = () => {
     return <>
         <Route exact path="/cocktails"><CocktailList /></Route>
-        <Route exact path="/cocktails/:cocktailId"><CocktailDetail /></Route>
+        <Route exact path="/cocktails/:cocktailId"><CocktailDetails /></Route>
     </>
 }
--- src/components/cocktail/CocktailDetails.js
+++ src/components/cocktail/CocktailDetails.js
@@ -1,16 +1,148 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { getCocktailById, getCocktailIngredients } from "./CocktailManager"
 import "./Cocktail.css"
 
-export const CocktailDetails = ({cocktail}) => {
+export const CocktailDetails = () => {
+    const params = useParams()
+    const [cocktailId, setCocktailId] = useState(parseInt(params.cocktailId));
+    const [cocktail, setCocktail] = useState({});
+    const [cocktailIngredients, setCocktailIngredients] = useState([]);
+
+    useEffect(() => {
+
+        getCocktailById(cocktailId).then((cocktail) => {
+            setCocktail(cocktail);
+        })
+
+        getCocktailIngredients().then((ingredients) => {
+            setCocktailIngredients(ingredients);
+        })
+
+    }, []);
+
     return (
-        <div className="cocktail">
-            <Link to={`/cocktails/${cocktail.id}`}><h3>{cocktail.name}</h3></Link>
-            <div className="cocktail-img-wrapper">
-                <img className="cocktail-img" src={cocktail.img_url} />
-            </div>
-            <div className="cocktail-info">
-                <p className="cocktail-description">{cocktail.description}</p>
+        <div className="main">
+            <div className="cocktail">
+                <h3>{cocktail.name}</h3>
+                <div className="cocktail-img-wrapper">
+                    <img className="cocktail-img" src={cocktail.img_url} />
+                </div>
+                <div className="cocktail-info">
+                    <p className="cocktail-description">{cocktail.description}</p>
+                    <div className="ingredients">
+                        {cocktailIngredients.map(ingredient => {
+                            if (ingredient.ingredient?.type.id == 1)
+                                return(
+                                    <div className="ingredient" key={ingredient.id}>
+                                        <div id="spacer-right">
+                                            <p>{parseFloat(ingredient.amount)}</p>
+                                            <p>{ingredient.unit.label}</p>
+                                        </div>
+                                        <div id="spacer-left">
+                                            <p>{ingredient.ingredient.name}</p>
+                                        </div>
+                                    </div>
+                                )
+                        })}
+                    </div>
+                    <div className="ingredients">
+                        {cocktailIngredients.map(ingredient => {
+                            if (ingredient.ingredient?.type.id == 2)
+                                return(
+                                    <div className="ingredient" key={ingredient.id} >
+                                        <div id="spacer-right">
+                                            <p>{parseFloat(ingredient.amount)}</p>
+                                            <p>{ingredient.unit.label}</p>
+                                        </div>
+                                        <div id="spacer-left">
+                                        <p>{ingredient.ingredient.name}</p>
+                                        </div>
+                                    </div>
+                                )
+                        })}
+                    </div>
+                    <div className="ingredients">
+                        {cocktailIngredients.map(ingredient => {
+                            if (ingredient.ingredient?.type.id == 7)
+                                return(
+                                    <div className="ingredient" key={ingredient.id}>
+                                        <div id="spacer-right">
+                                            <p>{parseFloat(ingredient.amount)}</p>
+                                            <p>{ingredient.unit.label}</p>
+                                        </div>
+                                        <div id="spacer-left">
+                                            <p>{ingredient.ingredient.name}</p>
+                                        </div>
+                                    </div>
+                                )
+                        })}
+                    </div>
+                    <div className="ingredients">
+                        {cocktailIngredients.map(ingredient => {
+                            if (ingredient.ingredient?.type.id == 6)
+                                return(
+                                    <div className="ingredient" key={ingredient.id}>
+                                        <div id="spacer-right">
+                                            <p>{parseFloat(ingredient.amount)}</p>
+                                            <p>{ingredient.unit.label}</p>
+                                        </div>
+                                        <div id="spacer-left">
+                                            <p>{ingredient.ingredient.name}</p>
+                                        </div>
+                                    </div>
+                                )
+                        })}
+                    </div>
+                    <div className="ingredients">
+                        {cocktailIngredients.map(ingredient => {
+                            if (ingredient.ingredient?.type.id == 5)
+                                return(
+                                    <div className="ingredient" key={ingredient.id}>
+                                        <div id="spacer-right">
+                                            <p>{parseFloat(ingredient.amount)}</p>
+                                            <p>{ingredient.unit.label}</p>
+                                        </div>
+                                        <div id="spacer-left">
+                                            <p>{ingredient.ingredient.name}</p>
+                                        </div>
+                                    </div>
+                                )
+                        })}
+                    </div>
+                    <div className="ingredients">
+                        {cocktailIngredients.map(ingredient => {
+                            if (ingredient.ingredient?.type.id == 3)
+                                return(
+                                    <div className="ingredient" key={ingredient.id}>
+                                        <div id="spacer-right">
+                                            <p>{parseFloat(ingredient.amount)}</p>
+                                            <p>{ingredient.unit.label}</p>
+                                        </div>
+                                        <div id="spacer-left">
+                                            <p>{ingredient.ingredient.name}</p>
+                                        </div>
+                                    </div>
+                                )
+                        })}
+                    </div>
+                    <div className="ingredients">
+                        {cocktailIngredients.map(ingredient => {
+                            if (ingredient.ingredient?.type.id == 4)
+                                return(
+                                    <div className="ingredient" key={ingredient.id}>
+                                        <div id="spacer-right">
+                                            <p>{parseFloat(ingredient.amount)} </p>
+                                            <p>{ingredient.unit?.label}</p>
+                                        </div>
+                                        <div id="spacer-left">
+                                            <p>{ingredient.ingredient.name}</p>
+                                        </div>
+                                    </div>
+                                )
+                        })}
+                    </div>
+                </div>
             </div>
         </div>
     )
